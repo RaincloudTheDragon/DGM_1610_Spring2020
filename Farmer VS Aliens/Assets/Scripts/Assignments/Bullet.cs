165b032fef81708ab12f8920953033f04fb0fb63
RCTD <dragonate2@gmail.com>
2020-02-26 17:30:53 -0700

    Added enemy health and bullet scripts

---
+++ Farmer VS Aliens/Assets/Scripts/Assignments/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 1;
+    public int time = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var hit = other.gameObject;
+            var health = hit.GetComponent<EnemyHealth>();
+
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+                Debug.Log("Ouch, you hit me!");
+            }
+        }
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+    //don't really need void update because we're cooler than it 😎
+
+}
---
+++ Farmer VS Aliens/Assets/Scripts/Assignments/EnemyHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth = 3;
+    public Transform spawnPoint;
+    //how many points do we get for destroying this enemy?
+    public int points = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //always start the game with max health!
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            //keep score at zero
+            currentHealth = 0;
+            print("Enemy is dead!");
+            //add points to score for killing enemy
+            //scoreManager.AddPoints(points);
+            //Move enemy from spawn point to restart
+            transform.position = spawnPoint.position;
+            transform.rotation = spawnPoint.rotation;
+            //Reset enemy health
+            currentHealth = maxHealth;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
