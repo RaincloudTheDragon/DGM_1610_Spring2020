3831c6bad1d83f89f26254a8c594d04eb12899cb
Pyro <nathanlindsay9@gmail.com>
2020-04-06 17:15:08 -0700

    Wander script work

--- Farmer VS Aliens/Assets/Scripts/Assignments/Wander.cs
+++ Farmer VS Aliens/Assets/Scripts/Assignments/Wander.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityENgine.AI;
 
 public class Wander : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+    private float timer;
+
+    // Use this for initialization
+    void OnEnable()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        timer = wanderTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newpos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = RandomNavSphere.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        UnityEngine.AI.NavMeshHit navHit;
+
+        UnityEngine.AI.NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
     }
 }
