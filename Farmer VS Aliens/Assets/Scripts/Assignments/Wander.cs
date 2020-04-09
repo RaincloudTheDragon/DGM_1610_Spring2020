bbb008e03fd3cae6919a7e81cf0f27db970f7481
Pyro <nathanlindsay9@gmail.com>
2020-04-08 17:19:33 -0700

    Fixed problems with Wander script
    2 compiler errors, one caused by the P in my second newPos not being capitalized, the other caused by Random likely being autocompleted to RandomNavSphere, which breaks that line of code. All fixed now

--- Farmer VS Aliens/Assets/Scripts/Assignments/Wander.cs
+++ Farmer VS Aliens/Assets/Scripts/Assignments/Wander.cs
@@ -21,19 +21,19 @@ public class Wander : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-/*         timer += Time.deltaTime;
+        timer += Time.deltaTime;
 
         if (timer >= wanderTimer)
         {
             Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
-            agent.SetDestination(newpos);
+            agent.SetDestination(newPos);
             timer = 0;
-        } */
+        }
     }
 
-/*     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
     {
-        Vector3 randDirection = RandomNavSphere.insideUnitSphere * dist;
+        Vector3 randDirection = Random.insideUnitSphere * dist;
 
         randDirection += origin;
 
@@ -42,5 +42,5 @@ public class Wander : MonoBehaviour
         UnityEngine.AI.NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
 
         return navHit.position;
-    } */
+    }
 }
